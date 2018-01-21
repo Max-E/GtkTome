Max-E/GtkTome

commit bed9513fb39abe55ee2671a18c92bfe7b8d5a22d

bulk page adding APIs. Can now add tabs in bulk for more speed during init.

example.py
@@ -52,10 +52,10 @@ addbutton = Gtk.Button (image = Gtk.Image (stock = Gtk.STOCK_ADD))
 tabbar.pack_end (addbutton, False, False, 0)
 
 total_append_count = 0
-def addtab (button = None, total_count_offset = 0):
+def addtab (button = None):
     global total_append_count
     total_append_count += 1
-    npages = tome.get_n_pages () + total_count_offset
+    npages = tome.get_n_pages ()
     tome.append_page ("Appended tab {} (tab {})".format (total_append_count, npages))
     return True
 addbutton.connect ("clicked", addtab)
@@ -71,8 +71,7 @@ def switchtab (tome, tab_number):
     return True
 tome.connect ("switch-tome-page", switchtab)
 
-for i in xrange (10000):
-    addtab (total_count_offset = 2)
+tome.bulk_append_pages (["Appended tab {} (tab {})".format (i, i + 2) for i in xrange (1000000)])
 
 tome.prepend_page ("Prepend tab title")
 tome.insert_page ("Insert tab title", 1)

__init__.py
@@ -116,8 +116,30 @@ class Tome (Gtk.Notebook):
     def insert_page (self, label_text, position):
         if position == -1:
             return self.append_page (label_text)
+        if position < 0:
+            position += self.get_n_pages ()
         self.labels.insert (position, label_text)
         self.emit ("tome-page-added", position)
+        self.right_idx = max (self._n_real_tabs (), position)
+        self.set_current_page (position)
+
+    def bulk_append_pages (self, label_texts):
+        inserted_page = self.get_n_pages () - 1 + min (1, len (label_texts))
+        self.labels += label_texts
+        self._update_tabs ()
+        self.set_current_page (inserted_page)
+
+    def bulk_prepend_pages (self, label_texts):
+        return self.bukl_insert_pages (label_texts, 0)
+
+    def bulk_insert_pages (self, label_texts, position):
+        if position == -1:
+            return self.bulk_append_pages (label_text)
+        if position < 0:
+            position += self.get_n_pages ()
+        self.labels = self.labels[:position] + label_texts + self.labels[position:]
+        for i in xrange (position, len (label_texts)):
+            self.emit ("tome-page-added", i)
         self.right_idx = max (self._n_real_tabs (), position)
         self.set_current_page (position)
 

commit 4d0174280754bfa582243be765b0481f911d4861

example app: unbreak counting for appended tabs
Fixes regression introduced in bed9513fb39abe55ee2671a18c92bfe7b8d5a22d

example.py
@@ -51,12 +51,12 @@ removebutton.connect ("clicked", removetab)
 addbutton = Gtk.Button (image = Gtk.Image (stock = Gtk.STOCK_ADD))
 tabbar.pack_end (addbutton, False, False, 0)
 
-total_append_count = 0
+total_append_count = 1000000
 def addtab (button = None):
     global total_append_count
-    total_append_count += 1
     npages = tome.get_n_pages ()
     tome.append_page ("Appended tab {} (tab {})".format (total_append_count, npages))
+    total_append_count += 1
     return True
 addbutton.connect ("clicked", addtab)
 